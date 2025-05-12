cc50de58ba2400ca0f4b74b4284f0e6ed8311a0e
Marco Tortolani <mjtorto@gmail.com>
2025-05-12 01:22:21 -0300

    fix

--- src/app/manager/projects/page.tsx
+++ src/app/manager/projects/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import { useRepairListStore } from '@/ stores/repair-list-store'
-import { useProjectsListStore } from '@/ stores/projects-list-store'
+import { useRepairListStore } from '@/stores/repair-list-store'
+import { useProjectsListStore } from '@/stores/projects-list-store'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import {
